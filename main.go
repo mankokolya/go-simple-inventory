c9f55effc03f5d76a75d625f6bab48fad818f096
Mykola Manko <mankokolya@gmail.com>
2025-04-08 19:46:05 +0300

    Add routes to the handlers

--- main.go
+++ main.go
@@ -2,14 +2,13 @@ package main
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"github.com/mankokolya/go-simple-inventory/routes"
 )
 
 func main() {
 	var app *fiber.App = fiber.New()
 
-	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("hello world!")
-	})
+	routes.SetupRoutes(app)
 
 	app.Listen("127.0.0.1:8080")
 }
