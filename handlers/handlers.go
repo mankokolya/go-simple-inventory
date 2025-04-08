df7b38bfb93ff9bd4399ce98d5df57a2f4b2b447
Mykola Manko <mankokolya@gmail.com>
2025-04-08 16:26:42 +0300

    Add controller to get inventory item by ID

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -1,6 +1,8 @@
 package handlers
 
 import (
+	"net/http"
+
 	"github.com/gofiber/fiber/v2"
 	"github.com/mankokolya/go-simple-inventory/models"
 	"github.com/mankokolya/go-simple-inventory/services"
@@ -13,5 +15,24 @@ func GetAllItems(c *fiber.Ctx) error {
 		Success: true,
 		Message: "All items data",
 		Data:    items,
+	})
+}
+
+func GetItemByID(c *fiber.Ctx) error {
+	var itemID string = c.Params("id")
+
+	item, err := services.GetItemById(itemID)
+
+	if err != nil {
+		return c.Status(http.StatusNotFound).JSON(models.Response[any]{
+			Success: false,
+			Message: err.Error(),
+		})
+	}
+
+	return c.JSON(models.Response[models.Item]{
+		Success: true,
+		Message: "item found",
+		Data:    item,
 	})
 }
